ca55177d055ee8adc9fb1dde0fbcbdb66c5c71c7
nate <augen@udel.edu>
2019-09-22 15:57:55 -0400

    debugging needed

--- api.php
+++ api.php
@@ -8,18 +8,56 @@
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
 
-    //this is a sample query which gets some data
-    $query = "SELECT rack, words FROM racks WHERE length=7 order by random() limit 1";
-
-    //this next line could actually be used to provide user_given input to the query to
-    //avoid SQL injection attacks
+    //get query response
+    //gets random rack length of 7
+    $maxRackLength = "7";
+    $query = "SELECT rack, words FROM racks WHERE length=".$maxRackLength." order by random() limit 1";
     $statement = $dbhandle->prepare($query);
     $statement->execute();
 
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     // $results is an array
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $results = array_merge($resuts,makeCombos($results["rack"]));
+    echo $results[0];
+    /*
+    // GETS WORDS FROM SUBRACKS
+    $subracks = makeCombos($results[0]["rack"]);
+    $numberOfSubracks = count($subracks);
+    $allWords = Array("words".$maxRackLength=>explode("@@",$results[0]["words"]));
+
+    for($i = 0; $i < $numberOfSubracks; $i++){
+        $subrack = $subracks[$i];
+        $subrackLen = strval(strlen($subrack)); // gets length of words that you could make with this rack
+
+        if($subrackLen == 1){
+            // check for a and I
+            if($subrack == "a" or $subrack == "i"){
+                insertIntoAllWords($subrack, $subrackLen, $allWords);
+            }
+        }
+        elseif($subrackLen == 0){
+            // do nothing!
+        }
+        else {
+            // prepare and get query
+            echo "<h1>HEY subrack:".$subrack."<h1>";
+            $getWordsQuery = "SELECT words FROM racks WHERE rack='".$subrack."'";
+            $statement0 = $dbhandle->prepare($getWordsQuery);
+            $statement0->execute();
+            $response = $statement0->fetchAll(PDO::FETCH_ASSOC); // gets key value array rack,words index 2
+
+            echo "<h1>HEY response:".gettype($response)."<h1>";
+
+            // ensures the subrack has some words
+            if($response[0] == null){
+                // do nothing!
+            }
+            elseif(array_key_exists("words",$response[0])){
+                insertIntoAllWords($response[0]["words"], $subrackLen, $allWords);
+            }
+        }
+
+    }
 
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
@@ -27,7 +65,22 @@
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode($results);
+    echo json_encode(array_merge($results[0],$allWords));
+    */
+    //
+    // HELPER FUNCTIONS
+    //
+
+    function insertIntoAllWords(String $istr, String $length, Array $allWords){
+        // checks if array key exists and handles insertion accordingly
+        $key = "words".$length;
+        if(array_key_exists($key,$allWords)){
+            $allWords[$key] = array_merge( $allWords[$key], explode("@@", $istr));
+        }
+        else{
+            $allWords[$key] = explode("@@", $istr);
+        }
+    }
 
     // creates an array with all the combos of chars in aString
     // combo strings will be in alphabetical order as long as input string is
