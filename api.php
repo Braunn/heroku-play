c49334c3169e772da4ed01750f0af9b286301125
nate <augen@udel.edu>
2019-09-22 16:05:44 -0400

    debugging

--- api.php
+++ api.php
@@ -18,7 +18,7 @@
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     // $results is an array
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($results);
+
     /*
     // GETS WORDS FROM SUBRACKS
     $subracks = makeCombos($results[0]["rack"]);
@@ -58,15 +58,15 @@
         }
 
     }
-
+    */
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode(array_merge($results[0],$allWords));
-    */
+    echo json_encode($results[0]);
+
     //
     // HELPER FUNCTIONS
     //
