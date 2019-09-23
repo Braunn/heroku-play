8ffdd2d0a47647da1c82b2660e97b3e1408effbf
nate <augen@udel.edu>
2019-09-23 01:00:57 -0400

    barebones

--- api.php
+++ api.php
@@ -26,16 +26,19 @@
     $numberOfSubracks = count($subracks);
     $allWords = Array("words".$maxRackLength=>explode("@@",$results["words"]));
 
-    $results = array_merge($results, Array("subracks"=>$subracks, "sublen"=> $numberOfSubracks), $allWords);
-    /*
+    $sArray = Array();
+    $tArray = Array("place"=>"holder");
+
     for($i = 0; $i < $numberOfSubracks; $i++){
         $subrack = $subracks[$i];
         $subrackLen = strval(strlen($subrack)); // gets length of words that you could make with this rack
 
+        $sArray = $sArray + Array($subrack =>$subrackLen);
+
         if($subrackLen == 1){
             // check for a and I
-            if($subrack == "a" or $subrack == "i"){
-                insertIntoAllWords($subrack, $subrackLen, $allWords);
+            if($subrack == "A" or $subrack == "I"){
+                insertIntoAllWords($subrack, $subrackLen);
             }
         }
         elseif($subrackLen == 0){
@@ -43,48 +46,52 @@
         }
         else {
             // prepare and get query
-            echo "<h1>HEY subrack:".$subrack."<h1>";
+
             $getWordsQuery = "SELECT words FROM racks WHERE rack='".$subrack."'";
+            //$getWordsQuery = "SELECT words FROM racks WHERE rack=''";
             $statement0 = $dbhandle->prepare($getWordsQuery);
             $statement0->execute();
             $response = $statement0->fetchAll(PDO::FETCH_ASSOC); // gets key value array rack,words index 2
 
-            echo "<h1>HEY response:".gettype($response)."<h1>";
+            /*
+            if(array_key_exists(0,$response)){
+                $tArray = $tArray + $response[0];
+            }
+            */
 
             // ensures the subrack has some words
-            if($response[0] == null){
-                // do nothing!
-            }
-            elseif(array_key_exists("words",$response[0])){
+            if(array_key_exists(0,$response)){
                 insertIntoAllWords($response[0]["words"], $subrackLen, $allWords);
             }
         }
-
     }
-    */
+
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode($results[0]);
+    echo json_encode($results + $allWords);
+    //echo json_encode($results + Array("subracks"=>$subracks));
 
     //
     // HELPER FUNCTIONS
     //
-    /*
-    function insertIntoAllWords(String $istr, String $length, Array $allWords){
+
+    function insertIntoAllWords(String $istr, String $length){
+        global $allWords;
+
         // checks if array key exists and handles insertion accordingly
         $key = "words".$length;
         if(array_key_exists($key,$allWords)){
-            $allWords[$key] = array_merge( $allWords[$key], explode("@@", $istr));
+            $allWords[$key] = $allWords[$key] + explode("@@", $istr);
         }
         else{
             $allWords[$key] = explode("@@", $istr);
         }
     }
-    */
+
 
     // creates an array with all the combos of chars in aString
     // combo strings will be in alphabetical order as long as input string is
