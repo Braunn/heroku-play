a8136f3eff52bea7049f8f0d943f59df3de6e7ac
nate <augen@udel.edu>
2019-09-30 11:28:56 -0400

    removed parameter from allWords call

--- api.php
+++ api.php
@@ -62,7 +62,7 @@
 
             // ensures the subrack has some words
             if(array_key_exists(0,$response)){
-                insertIntoAllWords($response[0]["words"], $subrackLen, $allWords);
+                insertIntoAllWords($response[0]["words"], $subrackLen);
             }
         }
     }
